f813509814f7485dc5fcfe5020dc0c9e20bd672c
feranstirman1 <37524727+feranstirman1@users.noreply.github.com>
2018-03-19 21:46:58 -0600

    Tarea1 con el codigo ya adentro
    La tarea 1 de POO ya con la carpeta src

---
+++ src/poo_misdatos/POO_MisDatos.java
@@ -0,0 +1,18 @@
+
+package poo_misdatos;
+
+/**
+ *
+ * @author Fer Anstirman 00024117
+ */
+public class POO_MisDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Nombres: Luis Fernando"+"\n"+"Apellidos: Anstirman Henriquez"+"\n"+"Fecha de Nacimiento: 26 de Octubre de 1999"+"\n"+"Carnet: 00024117"+"\n"+"Carrera: Ing. Informatica");
+    }
+    
+}
